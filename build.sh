e0600df1d0cdf663d53eee543c1965808bf80fcb
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-09-01 16:45:08 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,25 +5,15 @@ git clone https://github.com/muzei/muzei.git input
 cd input
 cp ../keystore.properties ./keystore.properties
 cp ../local.properties ./local.properties
-./gradlew :main:build :legacy-standalone:build
-mkdir ../outputdebuglegacy
+./gradlew :main:assembleRelease :legacy-standalone:assembleRelease
 mkdir ../outputreleaselegacy
 mkdir ../outputreleasemain
-mkdir ../outputdebugmain
-cp legacy-standalone/app/build/outputs/apk/debug/*.apk ../outputdebuglegacy
 cp legacy-standalone/app/build/outputs/apk/release/*.apk ../outputreleaselegacy
-cp main/app/build/outputs/apk/debug/*.apk ../outputdebugmain
 cp main/app/build/outputs/apk/release/*.apk ../outputreleasemain
-zipalign -p 4 ../outputdebuglegacy/*.apk ../outputdebuglegacy/aligned.apk
 zipalign -p 4 ../outputreleaselegacy/*.apk ../outputreleaselegacy/aligned.apk
 zipalign -p 4 ../outputreleasemain/*.apk ../outputreleasemain/aligned.apk
-zipalign -p 4 ../outputdebugmain/*.apk ../outputdebugmain/aligned.apk
-apksigner sign --ks-key-alias lob --ks ../sign.keystore --ks-pass pass:369852 --key-pass pass:369852 ../outputdebugmain/aligned.apk
 apksigner sign --ks-key-alias lob --ks ../sign.keystore --ks-pass pass:369852 --key-pass pass:369852 ../outputreleasemain/aligned.apk
 apksigner sign --ks-key-alias lob --ks ../sign.keystore --ks-pass pass:369852 --key-pass pass:369852 ../outputreleaselegacy/aligned.apk
-apksigner sign --ks-key-alias lob --ks ../sign.keystore --ks-pass pass:369852 --key-pass pass:369852 ../outputdebuglegacy/aligned.apk
 mkdir ../prebuilt
-cp ../outputdebugmain/aligned.apk ../prebuilt/main-debug.apk
 cp ../outputreleasemain/aligned.apk ../prebuilt/main-release.apk
 cp ../outputreleaselegacy/aligned.apk ../prebuilt/legacyaddon-release.apk
-cp ../outputdebuglegacy/aligned.apk ../prebuilt/legacyaddon-debug.apk
