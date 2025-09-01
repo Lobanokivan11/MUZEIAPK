3b10666cf3a5232fdc19bb79e69ea5752a52fa63
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2025-09-01 18:47:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ cp ../local.properties ./local.properties
 ./gradlew :main:assembleDebug :main:assembleRelease
 mkdir ../outputdebugmain
 mkdir ../outputreleasemain
-cp main/app/build/outputs/apk/debug/*.apk ../outputdebugmain
-cp main/app/build/outputs/apk/release/*.apk ../outputreleasemain
+cp main/build/outputs/apk/debug/*.apk ../outputdebugmain
+cp main/build/outputs/apk/release/*.apk ../outputreleasemain
 zipalign -p 4 ../outputreleasemain/*.apk ../outputreleasemain/aligned.apk
 zipalign -p 4 ../outputdebugmain/*.apk ../outputdebugmain/aligned.apk
 apksigner sign --ks-key-alias lob --ks ../sign.keystore --ks-pass pass:369852 --key-pass pass:369852 ../outputdebugmain/aligned.apk
